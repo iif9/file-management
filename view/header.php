6b115ff238fde2ef35ea8505c0cf43ff541e48eb
iif9 <f9.alhanini@gmail.com>
2022-04-27 22:42:50 -0400

    App File Management : Done AppBar

---
+++ view/header.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="ar">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+    <div class="header">
+        <div class="header-right">
+            <a href="#">ادارة الملفات</a>
+            <a href="#" class="logo">هوادف</a>
+        </div>
+    </div>
+</body>
+
+</html>
